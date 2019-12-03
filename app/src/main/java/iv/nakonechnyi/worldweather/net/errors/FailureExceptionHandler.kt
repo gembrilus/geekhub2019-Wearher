7a0edad19f0979d3c934122e10cd355102698a28
Максим Наконечный <gembrilus@gmail.com>
2019-12-03 12:01:29 +0200

    Add error messages

--- app/src/main/java/iv/nakonechnyi/worldweather/net/errors/FailureExceptionHandler.kt
+++ app/src/main/java/iv/nakonechnyi/worldweather/net/errors/FailureExceptionHandler.kt
@@ -1,14 +1,15 @@
 package iv.nakonechnyi.worldweather.net.errors
 
 import android.content.Context
+import iv.nakonechnyi.worldweather.R
 import iv.nakonechnyi.worldweather.utils.showErrorMessage
 import java.net.UnknownHostException
 
 class FailureExceptionHandler(private val ctx: Context) : OnFailureException{
     override fun handle(t: Throwable) {
         when(t){
-            is UnknownHostException -> showErrorMessage(ctx, "")
-            else -> showErrorMessage(ctx, "")
+            is UnknownHostException -> showErrorMessage(ctx, ctx.getString(R.string.no_network_connection))
+            else -> showErrorMessage(ctx, ctx.getString(R.string.unknown_error))
         }
     }
 }
--- app/src/main/java/iv/nakonechnyi/worldweather/utils/MessageUtils.kt
+++ app/src/main/java/iv/nakonechnyi/worldweather/utils/MessageUtils.kt
@@ -1,8 +1,8 @@
 package iv.nakonechnyi.worldweather.utils
 
 import android.content.Context
-
+import android.widget.Toast
 
 fun showErrorMessage(ctx: Context, message: String){
-
+    Toast.makeText(ctx, message, Toast.LENGTH_LONG).show()
 }
